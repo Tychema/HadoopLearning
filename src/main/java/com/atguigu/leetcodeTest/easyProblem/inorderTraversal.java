fac0eeaedb5ffcefc6e42a0b19eadd17c08f940b
Tychema <tychema@foxmail.com>
2023-03-06 20:50:26 +0800

    新的算法

---
+++ src/main/java/com/atguigu/leetcodeTest/easyProblem/inorderTraversal.java
@@ -0,0 +1,12 @@
+package com.atguigu.leetcodeTest.easyProblem;
+
+import java.util.List;
+import java.util.Stack;
+
+public class inorderTraversal {
+    public static void main(String[] args) {
+        Stack<Integer> integers = new Stack<>();
+        integers.empty();
+    }
+
+}
---
+++ src/main/java/com/atguigu/leetcodeTest/easyProblem/testThis.java
@@ -0,0 +1,9 @@
+package com.atguigu.leetcodeTest.easyProblem;
+
+public class testThis {
+    private Integer maxDeep=0;
+    public void main(String[] args) {
+        System.out.println(this.maxDeep);
+        Math.abs(Integer.MAX_VALUE);
+    }
+}
